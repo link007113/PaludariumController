7405d12ffdc5fd8918175a0313385dc8783df025
link007113 <43582369+link007113@users.noreply.github.com>
2022-10-26 08:30:21 +0200

    Add files via upload

--- PaludariumController.Core/Models/Light.cs
+++ PaludariumController.Core/Models/Light.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Drawing;
 using System.Text.Json.Serialization;
 using PaludariumController.Core.Attributes.Validators.Light;
 
@@ -6,44 +7,38 @@ namespace PaludariumController.Core.Models
 {
     public class Light
     {
-        [LightValue]
-        public string Red { get; set; }
-        [LightValue]
-        public string Blue { get; set; }
-        [LightValue]
-        public string Green { get; set; }
+        [Range(0, 255)]
+        public int Red { get; set; }
+        [Range(0, 255)]
+        public int Blue { get; set; }
+        [Range(0, 255)]
+        public int Green { get; set; }
 
-        public int Brightness { get { return (int.Parse(Red) + int.Parse(Green) + int.Parse(Blue)) / 3; } set { } }
+        public int Brightness { get { return Red + Green + Blue / 3; } set { } }
         public Light()
         {
-            this.Red = "255";
-            this.Blue = "255";
-            this.Green = "255";
+            this.Red = 255;
+            this.Blue = 255;
+            this.Green = 255;
         }
-        public Light(string red, string blue, string green)
-        {
-            this.Red = red.PadLeft(3, '0');
-            this.Blue = blue.PadLeft(3, '0');
-            this.Green = green.PadLeft(3, '0');
-        }
-
         public Light(int red, int blue, int green)
         {
-            this.Red = red.ToString().PadLeft(3, '0');
-            this.Blue = blue.ToString().PadLeft(3, '0');
-            this.Green = green.ToString().PadLeft(3, '0');
+            this.Red = red;
+            this.Blue = blue;
+            this.Green = green;
         }
+        
         public Light(Color color)
         {
-            this.Red = color.R.ToString().PadLeft(3, '0');
-            this.Blue = color.B.ToString().PadLeft(3, '0');
-            this.Green = color.G.ToString().PadLeft(3, '0');
+            this.Red = color.R;
+            this.Blue = color.B;
+            this.Green = color.G;
         }
 
 
         public static Color GetColor(Light light)
         {
-            return Color.FromArgb(int.Parse(light.Red), int.Parse(light.Green), int.Parse(light.Blue));
+            return Color.FromArgb(light.Red, light.Green, light.Blue);
         }
     }
 }
